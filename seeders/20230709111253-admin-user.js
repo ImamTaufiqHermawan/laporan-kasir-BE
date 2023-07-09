cc79ba122a3fc756c2bb4ed8ed5aea8bda34893c
Imam Taufiq Hermawan <imamtaufiq133@gmail.com>
2023-07-09 18:17:46 +0700

    seeder and config deployment

---
+++ seeders/20230709111253-admin-user.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Users', [
+      {
+        name: 'Lisma',
+        email: 'lisma@gmail.com',
+        password: '$2a$10$7nwqtWYWGDbeAvOU2sfyEeu0XLKCqxWT0A2NVkJ/Ml1.bfTW7pkZu',
+        phone_number: '08123456789',
+        role: "Manager",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Imam',
+        email: 'imam@gmail.com',
+        password: '$2a$10$7nwqtWYWGDbeAvOU2sfyEeu0XLKCqxWT0A2NVkJ/Ml1.bfTW7pkZu',
+        role: "Manager",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
